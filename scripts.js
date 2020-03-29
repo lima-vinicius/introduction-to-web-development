724359ac52afa07e168d994c297c6110d289b77a
viniciuslima-99 <vrpl@cin.ufpe.br>
2020-03-28 21:45:24 -0300

    feature(popup-form) :  Added the button scripts to open the popup

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function onOff() {
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("hide")
+
+    document
+        .querySelector("body")
+        .classList
+        .toggle("hideScroll")
+}
