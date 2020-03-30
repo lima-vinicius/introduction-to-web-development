fd45d3596a5a15e0c3a3cb1a412286661a275788
viniciuslima-99 <vrpl@cin.ufpe.br>
2020-03-30 19:07:42 -0300

    Feature(render-pages) : Created the routes to render the html pages and configured the static files

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
 // Imported the express in a variable to configure the server and use the functions
 const express = require("express");
+const path = require("path");
 const server = express();
 
 
-// Created the route /  
-server.get("/", function(req, res){
+// Configured static files (css, scripts, images)
+server.use('/pages', express.static(path.join(__dirname, '../client/pages')));
+server.use('/images', express.static(path.join(__dirname, '../client/images')));
+server.use('/style', express.static(path.join(__dirname, '../client/style')));
+server.use('/scripts', express.static(path.join(__dirname, '../client/scripts')));   
+
+
+// Created the route / and /ideas
+server.get('/', function(req, res){
     // req would be the request that the frontend makes and the res would be the answer  
-    console.log("Respondendo o terminal na rota /")
-    res.send('Respondendo a pagina web na rota /') 
-})
+    res.sendFile(path.join(__dirname, '../client/pages/index.html')) 
+});
+
+server.get('/ideas', function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/pages/ideas.html'))
+});
 
 // Used the listen function of express to connect the server to port 3000
-server.listen(3000)
+server.listen(3000); 
