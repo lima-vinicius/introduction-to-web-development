d08b7587214fc1f4cc9f10a54afb68c14c93f42b
viniciuslima-99 <vrpl@cin.ufpe.br>
2020-04-11 13:16:11 -0300

    feature(interconnect-with-database) : Created the function to not allow sending the modal with blank fields

--- client/scripts/scripts.js
+++ client/scripts/scripts.js
@@ -9,3 +9,28 @@ function onOff() {
         .classList
         .toggle("hideScroll")
 }
+
+function checkFields(event) {
+    const valuesToCheck = [
+        "title",
+        "category",
+        "img",
+        "description",
+        "url"
+    ];
+
+    const isEmpty = valuesToCheck.find((value) => {
+        const checkIfIsString = typeof event.target[value].value === "string";
+        const checkIfIsEmpty = !event.target[value].value.trim();
+
+        if(checkIfIsString && checkIfIsEmpty){
+            return true
+        };
+    });
+
+    if(isEmpty){
+        event.preventDefault();
+        alert("Por favor, preencha todos os campos!")
+    };
+};
+
